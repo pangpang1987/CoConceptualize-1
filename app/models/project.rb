171ccab9aa78c7808ed8841f7a614a41f6463ccf
pangpang1987 <pangpang.i.lily@gmail.com>
2012-10-29 15:45:04 +0800

    add canvas title

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,7 @@
 class Project < ActiveRecord::Base
 	has_many :project_users
 	has_many :users, :through => :project_users
-	attr_accessible :date_created, :id, :name, :assignuser
+	attr_accessible :date_created, :id, :name, :assignuser, :description
 
 	def assignuser
 	end
